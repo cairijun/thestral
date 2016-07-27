dff4e59bdff6c9351eed3e25f2af606dd500aae0
Richard Tsai <richard9404@gmail.com>
2016-07-27 19:53:07 +0800

    logging facility

---
+++ include/logging.h
@@ -0,0 +1,182 @@
+// Copyright 2016 Richard Tsai
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// @file
+/// Defines logging facility.
+#ifndef THESTRAL_LOG_H_
+#define THESTRAL_LOG_H_
+
+#include <cstdarg>
+#include <map>
+#include <mutex>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace thestral {
+namespace logging {
+
+class Logger;
+
+enum class Level { INFO, WARN, ERROR, FATAL };
+
+namespace impl {
+/// A record of logging. A LogRecord is comprised of a Level and a collection of
+/// attributes with string keys.
+class LogRecord {
+ public:
+  /// Sets an attribute of the log record.
+  void SetAttribute(const std::string& name, const std::string& value) {
+    attributes_[name] = value;
+  }
+
+  /// Returns the level of the log record.
+  Level GetLevel() const { return level_; }
+
+  /// Returns a constant reference to the attribute map.
+  const std::map<std::string, std::string>& GetAttributes() const {
+    return attributes_;
+  }
+
+ private:
+  friend class ::thestral::logging::Logger;
+
+  Level level_;
+  std::map<std::string, std::string> attributes_;
+
+  LogRecord(Level level, const std::map<std::string, std::string>& attributes)
+      : level_(level), attributes_(attributes) {}
+};
+
+/// Base class of log sinks. A log sink accepts LogRecord objects, and stores
+/// them into some place if the levels of the records are *not lower than* the
+/// level of the sink.
+class LogSinkBase {
+ public:
+  LogSinkBase() { SetFormat("[{time}][{level}][{logger_name}]: {message}"); }
+
+  LogSinkBase(const LogSinkBase&) = delete;
+
+  /// Returns a reference to the mutex of the sink. The mutex should be locked
+  /// before logging operations.
+  std::mutex& GetMutex() { return mtx_; }
+  /// Returns the level of the sink.
+  Level GetLevel() const { return level_; }
+  /// Sets the level of the sink.
+  void SetLevel(Level level) { level_ = level; }
+
+  /// Sets the format string of the sink. Patterns like `"{attr_name}"` in the
+  /// format will be replaced with the attribute values of key `attr_name` in
+  /// the log records when storing. If a required attribute is missing in the
+  /// record, an empty string will be used. The format string will be compiled
+  /// in a bid to improve performance.
+  void SetFormat(const std::string& format);
+  /// Pushes a log record into the sink. If the level of record is not lower
+  /// than the level of the sink, the record will be stored using StoreRecord().
+  void PushRecord(const LogRecord& record);
+
+ protected:
+  /// Component of the compiled format string. A component can generate a string
+  /// given an attribute map.
+  class CompiledFormatPart {
+   public:
+    CompiledFormatPart(const std::string& constant_or_key, bool is_constant)
+        : constant_or_key_(constant_or_key), is_constant_(is_constant) {}
+
+    std::string GetStringFromMap(
+        const std::map<std::string, std::string>& map) const;
+
+   private:
+    const std::string constant_or_key_;
+    bool is_constant_;
+  };
+
+  std::vector<CompiledFormatPart> compiled_format_;
+
+  /// Actually stores a record.
+  virtual void StoreRecord(const LogRecord& record) = 0;
+
+ private:
+  std::mutex mtx_;
+  Level level_ = Level::WARN;
+};
+
+}  // namespace impl
+
+/// Adds a log sink storing to a given file.
+impl::LogSinkBase& add_file_log_sink(const std::string& filename,
+                                     bool truncate = false);
+/// Adds a log sink writing to standard error file.
+impl::LogSinkBase& add_stderr_log_sink();
+
+class FatalEvent : public std::runtime_error {
+ public:
+  FatalEvent() : std::runtime_error("a fatal event is logged") {}
+};
+
+/// Logger class used to collect logs.
+class Logger {
+ public:
+  /// A proxy for setting attributes of created log record. The log record will
+  /// be pushed to log sinks during the destruction of the proxy object. The
+  /// users MUST NOT hold this proxy object.
+  class LogRecordProxy {
+   public:
+    ~LogRecordProxy() throw(FatalEvent);
+    LogRecordProxy& SetAttribute(const std::string& name,
+                                 const std::string& value) {
+      record_.SetAttribute(name, value);
+      return *this;
+    }
+
+   private:
+    friend class Logger;
+    impl::LogRecord record_;
+
+    explicit LogRecordProxy(impl::LogRecord&& record)
+        : record_(std::move(record)) {}
+  };
+
+  explicit Logger(const std::string& name);
+
+  /// Sets the name of the logger.
+  void SetName(const std::string& name);
+  /// Returns the name of the logger.
+  std::string GetName() const;
+  /// The an attribute of the logger. Log records created from this logger will
+  /// inherit this attribute.
+  void SetAttribute(const std::string& name, const std::string& value);
+
+  /// Create a log with the given level and message. The message will be
+  /// formated using the standard C-style formating syntax. When a log record is
+  /// created, some attributes will be added automatically: `logger_name` is the
+  /// name of this logger, `time` is the string representation of the local
+  /// creation time of the log, `level` is a symbol representing the level of
+  /// the log, and `message` is the log message. If a log of level FATAL is
+  /// created, a FatalEvent exception will be thrown.
+  LogRecordProxy Log(Level level, const char* format, ...) const;
+  LogRecordProxy Info(const char* format, ...) const;
+  LogRecordProxy Warn(const char* format, ...) const;
+  LogRecordProxy Error(const char* format, ...) const;
+  LogRecordProxy Fatal(const char* format, ...) const;
+
+ private:
+  std::map<std::string, std::string> attributes_;
+
+  LogRecordProxy Log(Level level, const char* format, va_list args) const;
+};
+
+}  // namespace logging
+}  // namespace thestral
+#endif  // THESTRAL_LOG_H_
---
+++ src/logging.cc
@@ -0,0 +1,228 @@
+// Copyright 2016 Richard Tsai
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// @file
+/// Implements logging facility.
+#include "logging.h"
+
+#include <cstdio>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <regex>
+#include <stdexcept>
+
+namespace thestral {
+namespace logging {
+
+namespace impl {
+
+std::string level_to_string(Level level) {
+  switch (level) {
+    case Level::INFO:
+      return "I";
+    case Level::WARN:
+      return "W";
+    case Level::ERROR:
+      return "E";
+    case Level::FATAL:
+      return "F";
+    default:
+      return "?";
+  }
+}
+
+std::string LogSinkBase::CompiledFormatPart::GetStringFromMap(
+    const std::map<std::string, std::string>& map) const {
+  if (is_constant_) {
+    return constant_or_key_;
+  } else {
+    auto iter = map.find(constant_or_key_);
+    if (iter != map.end()) {
+      return iter->second;
+    } else {
+      return "";
+    }
+  }
+}
+
+class FileLogSink : public LogSinkBase {
+ public:
+  FileLogSink(const std::string& filename,
+              std::ios_base::openmode mode = std::ios_base::out |
+                                             std::ios_base::app)
+      : ofs_(filename, mode) {}
+
+  void StoreRecord(const LogRecord& record) override {
+    for (auto& p : compiled_format_) {
+      ofs_ << p.GetStringFromMap(record.GetAttributes());
+    }
+    ofs_ << std::endl;
+  }
+
+ private:
+  std::ofstream ofs_;
+};
+
+class StdErrLogSink : public LogSinkBase {
+ public:
+  void StoreRecord(const LogRecord& record) override {
+    for (auto& p : compiled_format_) {
+      std::cerr << p.GetStringFromMap(record.GetAttributes());
+    }
+    std::cerr << std::endl;
+  }
+};
+
+void LogSinkBase::SetFormat(const std::string& format) {
+  std::regex re("\\{([_[:alnum:]]+?)\\}");
+  std::sregex_iterator search_iter(format.begin(), format.end(), re);
+  std::sregex_iterator search_end;
+
+  compiled_format_.clear();
+  auto last_end = format.cbegin();
+  while (search_iter != search_end) {
+    auto m = *search_iter;
+    if (last_end != m[0].first) {  // constant string before current match
+      compiled_format_.emplace_back(std::string(last_end, m[0].first), true);
+    }
+    compiled_format_.emplace_back(m[1], false);  // m[1] is the current attr key
+    last_end = m[0].second;
+    ++search_iter;
+  }
+
+  if (last_end != format.cend()) {
+    compiled_format_.emplace_back(std::string(last_end, format.cend()), true);
+  }
+}
+
+void LogSinkBase::PushRecord(const LogRecord& record) {
+  if (record.GetLevel() >= level_) {
+    StoreRecord(record);
+  }
+}
+
+std::vector<std::unique_ptr<LogSinkBase>> g_log_sinks;
+
+}  // namespace impl
+
+impl::LogSinkBase& add_file_log_sink(const std::string& filename,
+                                     bool truncate) {
+  impl::g_log_sinks.emplace_back(new impl::FileLogSink(
+      filename, std::ios_base::out |
+                    (truncate ? std::ios_base::trunc : std::ios_base::app)));
+  return *impl::g_log_sinks.back();
+}
+
+impl::LogSinkBase& add_stderr_log_sink() {
+  impl::g_log_sinks.emplace_back(new impl::StdErrLogSink());
+  return *impl::g_log_sinks.back();
+}
+
+Logger::Logger(const std::string& name) { SetName(name); }
+
+void Logger::SetName(const std::string& name) {
+  attributes_["logger_name"] = name;
+}
+
+std::string Logger::GetName() const { return attributes_.at("logger_name"); }
+
+Logger::LogRecordProxy Logger::Log(Level level, const char* format,
+                                   va_list args) const {
+  // we may need to call vsnprintf twice, but a va_list can be used only once
+  va_list args_clone;
+  va_copy(args_clone, args);
+  // only unique_ptr can manage array in C++11, DO NOT use share_ptr here
+  // 128 bytes should be sufficient in most cases
+  std::unique_ptr<char[]> buf(new char[128]);
+  auto msg_len = std::vsnprintf(buf.get(), 128, format, args);
+
+  if (msg_len > 127) {  // buffer is not large enough
+    buf.reset(new char[msg_len + 1]);
+    std::vsnprintf(buf.get(), msg_len + 1, format, args_clone);
+  }
+  va_end(args_clone);
+
+  auto attributes = attributes_;
+  attributes["message"] = buf.get();
+
+  char time_buf[64];
+  std::time_t t = std::time(nullptr);
+  std::strftime(time_buf, sizeof(time_buf), "%x %X", std::localtime(&t));
+  attributes["time"] = time_buf;
+
+  attributes["level"] = impl::level_to_string(level);
+
+  return LogRecordProxy(impl::LogRecord(level, attributes));
+}
+
+Logger::LogRecordProxy::~LogRecordProxy() throw(FatalEvent) {
+  // the incremental locking here ensures records are in the same order in all
+  // sinks. But be cautious about the order of locking to avoid deadlock!
+  std::vector<std::unique_lock<std::mutex>> lock_guards;
+  for (auto& sink : impl::g_log_sinks) {
+    lock_guards.emplace_back(sink->GetMutex());
+    sink->PushRecord(record_);
+  }
+
+  // Throwing in dtor is normally bad. But we required the users not to hold the
+  // proxy object. So it should be okay here.
+  if (record_.GetLevel() == Level::FATAL) {
+    throw FatalEvent();
+  }
+}
+
+Logger::LogRecordProxy Logger::Log(Level level, const char* format, ...) const {
+  va_list args;
+  va_start(args, format);
+  auto record = Log(level, format, args);
+  va_end(args);
+  return record;
+}
+
+Logger::LogRecordProxy Logger::Info(const char* format, ...) const {
+  va_list args;
+  va_start(args, format);
+  auto record = Log(Level::INFO, format, args);
+  va_end(args);
+  return record;
+}
+
+Logger::LogRecordProxy Logger::Warn(const char* format, ...) const {
+  va_list args;
+  va_start(args, format);
+  auto record = Log(Level::WARN, format, args);
+  va_end(args);
+  return record;
+}
+
+Logger::LogRecordProxy Logger::Error(const char* format, ...) const {
+  va_list args;
+  va_start(args, format);
+  auto record = Log(Level::ERROR, format, args);
+  va_end(args);
+  return record;
+}
+
+Logger::LogRecordProxy Logger::Fatal(const char* format, ...) const {
+  va_list args;
+  va_start(args, format);
+  auto record = Log(Level::FATAL, format, args);
+  va_end(args);
+  return record;
+}
+
+}  // namespace logging
+}  // namespace thestral
